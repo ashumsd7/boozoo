721b5ed1cc63595af7715fe3d3a48615de749ad1
Aashutosh Anand Tiwari <40313523+ashumsd7@users.noreply.github.com>
2019-12-26 01:37:07 +0530

    Add files via upload

---
+++ js/magzines/weekly.js
@@ -0,0 +1,257 @@
+var nav_html= `
+<header>
+<div class="container">
+    <nav class="navbar navbar-expand-lg navbar-light ">
+        <a class="navbar-brand" href="#">FREE PDF BOOKS</a>
+
+        <ul class="navbar-nav ml-auto">
+
+
+
+
+
+
+
+        </ul>
+
+</div>
+</nav>
+
+</div>
+</header>
+
+
+
+<div class="quick-link">
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <!-- <a class="navbar-brand" href="#">Navbar</a> -->
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+
+            <li class="nav-item ">
+                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+            </li>
+
+
+
+
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                    data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Magzines
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="">Weekly</a>
+                    <a class="dropdown-item" href="#">Monthly</a>
+                    <a class="dropdown-item" href="#">Yearly</a>
+                    <a class="dropdown-item" href="#">GK</a>
+                    <a class="dropdown-item" href="#">GS</a>
+                    <a class="dropdown-item" href="#">Sports</a>
+                    <a class="dropdown-item" href="#">Health</a>
+                    <a class="dropdown-item" href="#">Computer & Electronics</a>
+                    <a class="dropdown-item" href="#">LifeStyle</a>
+                    <a class="dropdown-item" href="#">Travel & Places</a>
+                    <a class="dropdown-item" href="#">Cooking & Food</a>
+                    <a class="dropdown-item" href="#">Men's Magzines</a>
+                    <a class="dropdown-item" href="#">Women's Magzines</a>
+                    <a class="dropdown-item" href="#">Fashion & Style</a>
+                    <a class="dropdown-item" href="#">Entertainment & TV</a>
+                    <a class="dropdown-item" href="#">News & Politics</a>
+
+                    <div class="dropdown-divider"></div>
+                    <a class="dropdown-item" href="#">lIST OF ALL BOOKS</a>
+                </div>
+            </li>
+
+
+
+
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                    data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Novels
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="#">Literature</a>
+                    <a class="dropdown-item" href="#">Fiction</a>
+                    <a class="dropdown-item" href="#">Non-Fiction</a>
+                    <a class="dropdown-item" href="#">For Children</a>
+                    <div class="dropdown-divider"></div>
+                    <a class="dropdown-item" href="#">lIST OF ALL BOOKS</a>
+                </div>
+            </li>
+
+
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                    data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Exam Prep
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="#">UPSC</a>
+                    <a class="dropdown-item" href="#">JEE</a>
+                    <a class="dropdown-item" href="#">GATE</a>
+                    <a class="dropdown-item" href="#">CAT</a>
+                    <a class="dropdown-item" href="#">AIIMS</a>
+                    <a class="dropdown-item" href="#">CLAT</a>
+                    <a class="dropdown-item" href="#">UGC NET</a>
+                    <a class="dropdown-item" href="#">IES</a>
+                    <a class="dropdown-item" href="#">NDA</a>
+                    <a class="dropdown-item" href="#">CA</a>
+
+
+                    <div class="dropdown-divider"></div>
+                    <a class="dropdown-item" href="#">lIST OF ALL BOOKS</a>
+                </div>
+            </li>
+
+
+
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                    data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Academics
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="#">CBSE Class 1</a>
+                    <a class="dropdown-item" href="#">CBSE Class 2</a>
+                    <a class="dropdown-item" href="#">CBSE Class 3</a>
+                    <a class="dropdown-item" href="#">CBSE Class 4</a>
+                    <a class="dropdown-item" href="#">CBSE Class 5</a>
+                    <a class="dropdown-item" href="#">CBSE Class 6</a>
+                    <a class="dropdown-item" href="#">CBSE Class 7</a>
+                    <a class="dropdown-item" href="#">CBSE Class 8</a>
+                    <a class="dropdown-item" href="#">CBSE Class 9</a>
+                    <a class="dropdown-item" href="#">CBSE Class 10</a>
+                    <a class="dropdown-item" href="#">CBSE Class 11</a>
+                    <a class="dropdown-item" href="#">CBSE Class 12</a>
+
+                    <div class="dropdown-divider"></div>
+                    <a class="dropdown-item" href="#">lIST OF ALL BOOKS</a>
+                </div>
+            </li>
+
+
+
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                    data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Relegious
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="#">Hindu</a>
+                    <a class="dropdown-item" href="#">Muslim</a>
+                    <a class="dropdown-item" href="#">Shikh</a>
+                    <a class="dropdown-item" href="#">Christian</a>
+                    <a class="dropdown-item" href="#">Jain</a>
+                    <a class="dropdown-item" href="#">Buddha</a>
+
+
+                    <div class="dropdown-divider"></div>
+                    <a class="dropdown-item" href="#">lIST OF ALL BOOKS</a>
+                </div>
+            </li>
+
+
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                    data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    GRADUATION
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="#">B.A.</a>
+                    <a class="dropdown-item" href="#">B.COM</a>
+                    <a class="dropdown-item" href="#">B.ED</a>
+                    <a class="dropdown-item" href="#">B.C.A</a>
+                    <a class="dropdown-item" href="#">B.E/B.TECH</a>
+                    <a class="dropdown-item" href="#">L.L.B</a>
+                    <a class="dropdown-item" href="#">M.B.B.S</a>
+                    <a class="dropdown-item" href="#">B.SC.</a>
+                    <a class="dropdown-item" href="#">B.PHARMA</a>
+                    <a class="dropdown-item" href="#">B.B.A.</a>
+                    <div class="dropdown-divider"></div>
+                    <a class="dropdown-item" href="#">lIST OF ALL BOOKS</a>
+                </div>
+            </li>
+
+
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                    data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    POST GRADUATION
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="#">M.A.</a>
+                    <a class="dropdown-item" href="#">M.COM</a>
+                    <a class="dropdown-item" href="#">M.ED</a>
+                    <a class="dropdown-item" href="#">M.C.A</a>
+                    <a class="dropdown-item" href="#">M.E/M.TECH</a>
+                    <a class="dropdown-item" href="#">L.L.M</a>
+
+                    <a class="dropdown-item" href="#">M.SC.</a>
+                    <a class="dropdown-item" href="#">M.PHARMA</a>
+                    <a class="dropdown-item" href="#">M.B.A.</a>
+                    <div class="dropdown-divider"></div>
+                    <a class="dropdown-item" href="#">lIST OF ALL BOOKS</a>
+                </div>
+            </li>
+
+
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                    data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    SPRITUAL
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="#">SPRITUAL BOOKS</a>
+
+                    <!-- <div class="dropdown-divider"></div>
+                    <a class="dropdown-item" href="#">lIST OF ALL BOOKS</a> -->
+                </div>
+            </li>
+
+
+
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                    data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    CS BOOKS
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="#">CODING BOOKS</a>
+                    <a class="dropdown-item" href="#">DATA STRUCTURE</a>
+                    <a class="dropdown-item" href="#">PROGRAMMING LANGUAGES</a>
+                    <a class="dropdown-item" href="#">ALGORITHMS</a>
+
+                    <!-- <div class="dropdown-divider"></div>
+                    <a class="dropdown-item" href="#">lIST OF ALL BOOKS</a> -->
+                </div>
+            </li>
+
+
+
+
+
+
+
+
+
+
+
+
+        </ul>
+    </div>
+</nav>
+
+
+`
+
+
+
+document.getElementById('my_common_nav').innerHTML= nav_html;
